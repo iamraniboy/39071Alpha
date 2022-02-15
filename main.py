59f73c50c09299cd56cd18ac17e2f3dad7a2d333
iamraniboy <75585799+iamraniboy@users.noreply.github.com>
2022-02-15 10:20:35 -0500

    Update main.py
    adding input 10 button

--- main.py
+++ main.py
@@ -138,3 +138,15 @@ while loop_running:
     else:
         print("Invalid Command")
         
+
+        
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+def button_callback(channel):
+    
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+
+while True: # Run forever
+    if GPIO.input(10) == GPIO.RISING:
